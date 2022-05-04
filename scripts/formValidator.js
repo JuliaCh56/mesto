84db7ae17642ba285d73cabd82d29f1198ea70f6
JuliaCh56 <chugunovajulia56@gmail.com>
2022-05-04 14:16:30 +0300

    проект_7_проверка_3

--- scripts/formValidator.js
+++ scripts/formValidator.js
@@ -66,10 +66,10 @@ _hasInvalidInput() {
   _toggleButtonState = () => {
     if (this._hasInvalidInput(this._inputList)) {
       this._buttonElement.classList.add(this._inactiveButtonClass); 
-      this._buttonElement.setAttribute('disable', true); 
+      this._buttonElement.setAttribute('disabled', true); 
     } else {
       this._buttonElement.classList.remove(this._inactiveButtonClass);
-      this._buttonElement.removeAttribute('disable'); 
+      this._buttonElement.removeAttribute('disabled'); 
     }
   };
 
